2dba0234cd3ea51a24c58968eb9ae80ea4e52d7a
Daigo Shitara <stain.witness+github@gmail.com>
2022-04-24 18:26:28 +0900

    rm unused code

--- static/site.js
+++ static/site.js
@@ -1,14 +1,4 @@
-const $nav = $('#nav')
 const $main = $('main')
-
-window.addEventListener(
-  'hashchange',
-  function (ev) {
-    const hash = ev.target.location.hash
-    console.log(hash)
-  },
-  false
-)
 
 $('#nav-switch').on('click', () => {
   $main.toggleClass('with-index')
